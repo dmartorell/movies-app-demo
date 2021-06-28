0b84ebf3e9f35068b677dee8a8311e60e9529c7b
Daniel Martorell <dani.martorell@gmail.com>
2021-06-28 23:26:37 +0200

    Style HeroSection

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -2,24 +2,43 @@ import React from 'react';
 import Button from '../Button';
 import './style.scss';
 
-const HeroSection = () => (
-  <section>
-    <div className="container">
-      <article className="container__left-side">
-        <h1>
-          Dare to Play higher
-        </h1>
-        <h3>
-          Find movies and TV Shows that bring your mood back.
-        </h3>
-        <Button className="list-btn" onClick={() => {}}>Explore TV Shows</Button>
-        <Button className="list-btn" onClick={() => {}}>Explore Movies</Button>
+const HeroSection = () => {
+  const heroImage = 'https://i.ibb.co/VL959Vg/hero-image4.png';
 
-      </article>
-      <article className="container__right-side" />
+  return (
+    <section>
+      <div className="container">
+        <article className="container__left-side">
+          <h1 className="hero-title">
+            Dare
+            {' '}
+            <br />
+            to Play
+            {' '}
+            <br />
+            higher
+          </h1>
+          <h3 className="hero-subtitle">
+            2000+ movies and TV Shows
+            {' '}
+            <br />
+            to bring your
+            {' '}
+            <span className="hero-subtitle__highlight"> mood back.</span>
+          </h3>
+          <div className="buttons">
+            <Button className="main-btn main-btn--selected" onClick={() => { console.log('hi'); }}>Movies</Button>
+            <Button className="main-btn" onClick={() => {}}>TV Shows</Button>
+          </div>
 
-    </div>
-  </section>
-);
+        </article>
+        <article className="container__right-side">
+          <img src={heroImage} className="hero-image" alt="shapla main ilustration" />
+        </article>
+
+      </div>
+    </section>
+  );
+};
 
 export default HeroSection;
--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -4,7 +4,6 @@ import './style.scss';
 
 const Landing = () => (
   <>
-    <p>you are at landing</p>
     <HeroSection />
   </>
 );
