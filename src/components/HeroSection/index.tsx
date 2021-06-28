8f011ca4d5f0abbb34d6cbcca122c7c6ec68c6c6
Daniel Martorell <dani.martorell@gmail.com>
2021-06-29 00:24:01 +0200

    Toggle styles when button is clicked at HeroSection

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../Button';
 import './style.scss';
 
 const HeroSection = () => {
   const heroImage = 'https://i.ibb.co/VL959Vg/hero-image4.png';
+  const [tvIsSelected, setTvIsSelected] = useState(false);
+
+  const toggleButton = () => {
+    setTvIsSelected(!tvIsSelected);
+  };
+
+  const handleClick = () => {
+    toggleButton();
+  };
 
   return (
     <section>
@@ -19,7 +28,7 @@ const HeroSection = () => {
             higher
           </h1>
           <h3 className="hero-subtitle">
-            2000+ movies and TV Shows
+            2000+ Movies and TV Shows
             {' '}
             <br />
             to bring your
@@ -27,15 +36,33 @@ const HeroSection = () => {
             <span className="hero-subtitle__highlight"> mood back.</span>
           </h3>
           <div className="buttons">
-            <Button className="main-btn main-btn--selected" onClick={() => { console.log('hi'); }}>Movies</Button>
-            <Button className="main-btn" onClick={() => {}}>TV Shows</Button>
+            <Button
+              className={
+              tvIsSelected
+                ? 'main-btn'
+                : 'main-btn main-btn--selected'
+            }
+              disabled={!tvIsSelected}
+              onClick={handleClick}
+            >
+              Movies
+            </Button>
+            <Button
+              className={
+              tvIsSelected
+                ? 'main-btn main-btn--selected'
+                : 'main-btn'
+              }
+              disabled={tvIsSelected}
+              onClick={handleClick}
+            >
+              TV Shows
+            </Button>
           </div>
-
         </article>
         <article className="container__right-side">
           <img src={heroImage} className="hero-image" alt="shapla main ilustration" />
         </article>
-
       </div>
     </section>
   );
