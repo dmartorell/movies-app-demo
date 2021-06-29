4076543b96a7fbd0c26204d2e5b76e7854475ca9
Daniel Martorell <dani.martorell@gmail.com>
2021-06-29 21:36:27 +0200

    Apply truncate text on movie/tvShow title

--- src/components/MoviesList/index.tsx
+++ src/components/MoviesList/index.tsx
@@ -13,8 +13,10 @@ const MoviesList = ({ list }: {list : Movie[]}) => {
             && list.map((item : any) => (
               <li className="list-item" key={item.id}>
                 <img className="list-item__image" src={`${IMAGE_URL}${item.poster_path}`} alt="list-item-pic" />
-                <p className="list-item__title">{item.title}</p>
-                <p className="list-item__vote-average">{item.vote_average}</p>
+                <div className="list-item__info">
+                  <p className="info-title">{item.title}</p>
+                  <p className="info-votes">{item.vote_average}</p>
+                </div>
               </li>
             ))
           }
