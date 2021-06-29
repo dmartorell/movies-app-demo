2b5eb48e82ebafe56d18c9aedb7df36c2f13ddc4
Daniel Martorell <dani.martorell@gmail.com>
2021-06-29 12:41:55 +0200

    Fix warning error when using useSelector in HeroSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import configureStore from './redux/store';
 import Navbar from './components/Navbar';
+import configureStore from './redux/store';
 import Landing from './components/Landing';
 import NotFound from './components/NotFound';
 
