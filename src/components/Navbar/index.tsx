5520fdddfcec30bd046f1349e041ebccdcce643e
Daniel Martorell <dani.martorell@gmail.com>
2021-07-01 01:05:25 +0200

    Add external link to linkedin profile

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,24 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.scss';
 
-const Navbar = () => (
-  <nav>
-    <ul className="nav-list">
-      <li className="nav-list__item">
-        <Link className="logo" to="/">
-          nonomo
-        </Link>
-      </li>
-      <li className="nav-list__item">
-        <ul className="nav-nested-list">
-          <li><Link className="nav-nested-list__item" to="/">About</Link></li>
-          <li><Link className="nav-nested-list__item" to="/">Features</Link></li>
-          <li><Link className="nav-nested-list__item" to="/">Pricing</Link></li>
-        </ul>
-      </li>
-      <li className="nav-list__item"><Link className="contactUs" to="/">Contact Us</Link></li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const personalLinkedin = 'https://www.linkedin.com/in/daniel-martorell/';
+  return (
+    <nav>
+      <ul className="nav-list">
+        <li className="nav-list__item">
+          <Link className="logo" to="/">
+            nonomo
+          </Link>
+        </li>
+        <li className="nav-list__item">
+          <ul className="nav-nested-list">
+            <li><Link className="nav-nested-list__item" to="/">About</Link></li>
+            <li><Link className="nav-nested-list__item" to="/">Features</Link></li>
+            <li><Link className="nav-nested-list__item" to="/">Pricing</Link></li>
+          </ul>
+        </li>
+        <li className="nav-list__item"><a className="contactUs" target="_blank" href={personalLinkedin} rel="noreferrer">Contact Us</a></li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
