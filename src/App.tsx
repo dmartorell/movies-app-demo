3dcfce5da0cae464852a9600b93586b10de75de3
Daniel Martorell <dani.martorell@gmail.com>
2021-06-30 08:47:41 +0200

    Create Detail component and make it common to Tv and Movies

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import configureStore from './redux/store';
 import Landing from './components/Landing';
-import MovieDetail from './components/MovieDetail';
-import TvDetail from './components/TvDetail';
+import Detail from './components/Detail';
 import NotFound from './components/NotFound';
-
 import './App.scss';
 
 function App() {
@@ -18,9 +16,8 @@ function App() {
         <main className="main-container">
           <Switch>
             <Route exact path="/" component={Landing} />
-            <Route path="/movie/:movieId" component={MovieDetail} />
-            <Route path="/tv/:tvId" component={TvDetail} />
-
+            <Route path="/movie/:id" component={Detail} />
+            <Route path="/tv/:id" component={Detail} />
             <Route component={NotFound} />
           </Switch>
         </main>
