ec801d26be3a9b20ec5cc0e5d1701a48b9dd0503
Daniel Martorell <dani.martorell@gmail.com>
2021-06-30 22:02:20 +0200

    Fix bug : Message 'You may also like...' was printed even without data to show

--- src/components/SimilarItems/index.tsx
+++ src/components/SimilarItems/index.tsx
@@ -11,22 +11,28 @@ const SimilarItems = ({ data }: {data: Movie[]}) => {
 
   return (
     <section>
-      <h2 className="similar-title">
-        You might also like...
-      </h2>
-      <ul className="similar-list">
-        {
-          data
-            ? data.map((item : Movie) => (
-              <li className="similar-item" key={item.id}>
-                <Link to={`/${type}/${item.id}`}>
-                  <img className="similar-item__poster" src={`${IMAGE_URL}${item.poster_path}`} alt="poster" />
-                </Link>
-              </li>
-            ))
-            : null
+      {
+        data.length
+          ? (
+            <>
+              <h2 className="similar-title">
+                You might also like...
+              </h2>
+              <ul className="similar-list">
+                {
+                    data.map((item : Movie) => (
+                      <li className="similar-item" key={item.id}>
+                        <Link to={`/${type}/${item.id}`}>
+                          <img className="similar-item__poster" src={`${IMAGE_URL}${item.poster_path}`} alt="poster" />
+                        </Link>
+                      </li>
+                    ))
+                }
+              </ul>
+            </>
+          )
+          : null
         }
-      </ul>
     </section>
   );
 };
