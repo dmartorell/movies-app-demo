78940c3b8611ba2e3a400f61b6b753d960e249f1
Daniel Martorell <dani.martorell@gmail.com>
2021-07-01 23:36:10 +0200

    Remove code smells

--- src/redux/reducers/moviesReducer.ts
+++ src/redux/reducers/moviesReducer.ts
@@ -2,11 +2,9 @@ import actionTypes from '../actions/actionTypes';
 import PopularMovies from '../../types/popularMovies';
 
 function moviesReducer(popularMovies = [], action: {type: string, popularMovies: PopularMovies}) {
-  switch (action.type) {
-    case actionTypes.LOAD_MOVIES:
-      return action.popularMovies;
-    default:
-      return popularMovies;
+  if (action.type === actionTypes.LOAD_MOVIES) {
+    return action.popularMovies;
   }
+  return popularMovies;
 }
 export default moviesReducer;
--- src/redux/reducers/selectedItemReducer.ts
+++ src/redux/reducers/selectedItemReducer.ts
@@ -2,11 +2,9 @@ import actionTypes from '../actions/actionTypes';
 import Detail from '../../types/detail';
 
 function selectedItemReducer(selectedItem = {}, action: {type: string, selectedItem: Detail}) {
-  switch (action.type) {
-    case actionTypes.LOAD_DETAIL:
-      return action.selectedItem;
-    default:
-      return selectedItem;
+  if (action.type === actionTypes.LOAD_DETAIL) {
+    return action.selectedItem;
   }
+  return selectedItem;
 }
 export default selectedItemReducer;
