3dc14f300cdae16fc8b69c665f7c4fc5a04816a8
Daniel Martorell <dani.martorell@gmail.com>
2021-07-02 10:45:14 +0200

    Implement testing : HeroSection clicking on buttons

--- src/components/HeroSection/heroSection.test.tsx
+++ src/components/HeroSection/heroSection.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeroSection from './index';
-import { render } from '../../utils/test-utils';
+import { render, screen, fireEvent } from '../../utils/test-utils';
 
 describe('Given a HeroSection component', () => {
   describe('When is rendered', () => {
@@ -8,6 +8,17 @@ describe('Given a HeroSection component', () => {
       const { container } = render(<HeroSection />);
       const chooseTag = container.querySelector('.hero-image');
       expect(chooseTag).toBeDefined();
+    });
+  });
+});
+describe('Given a HeroSection component', () => {
+  describe('When TvShows button is clicked', () => {
+    test('Then class \'--selected\' should be added', () => {
+      render(<HeroSection />);
+      const buttons = screen.getAllByRole('button');
+      const tvShowsBtn = buttons.find((button) => button.textContent === 'TV Shows');
+      if (tvShowsBtn) fireEvent.click(tvShowsBtn);
+      expect(tvShowsBtn?.classList.contains('main-btn--selected')).toBe(true);
     });
   });
 });
