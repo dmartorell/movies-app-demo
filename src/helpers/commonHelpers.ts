60c0bc76921fbbd337c96bc9bdab3dcff386a458
Daniel Martorell <dani.martorell@gmail.com>
2021-07-05 22:27:34 +0200

    Change getAverageRunTime func to avoid bug when empty arrray provided

--- src/helpers/commonHelpers.ts
+++ src/helpers/commonHelpers.ts
@@ -1,6 +1,6 @@
 export const getAverageRunTime = (times:number[]) => {
-  const sum = times?.reduce((acc : number, val : number) => acc + val);
-  return sum / times?.length;
+  const sum = times?.length && times.reduce((acc : number, val : number) => acc + val);
+  return (sum / times?.length) || 'unknown';
 };
 
 export const getTypeFromLocation = () => window.location.pathname.split('/')[1];
