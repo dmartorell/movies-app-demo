8d8c47d69ffb080ec1baf50dc91d19137382e6d8
Daniel Martorell <dani.martorell@gmail.com>
2021-07-05 22:26:49 +0200

    Add default poster in case there is no data available

--- src/components/TvShowsList/index.tsx
+++ src/components/TvShowsList/index.tsx
@@ -5,6 +5,7 @@ import TvShow from '../../types/tvShow';
 
 const TvShowsList = ({ list }: {list : TvShow[] }) => {
   const IMAGE_URL = 'https://image.tmdb.org/t/p/original/';
+  const defaultPoster = 'https://i.ibb.co/vYbnYLQ/Captura-de-pantalla-2021-07-05-a-las-21-52-36.jpg';
   return (
     <section>
       <ul className="list">
@@ -13,7 +14,7 @@ const TvShowsList = ({ list }: {list : TvShow[] }) => {
             && list.map((item : TvShow) => (
               <li className="list-item" key={item.id}>
                 <Link to={`/tv/${item.id}`}>
-                  <img className="list-item__image" src={`${IMAGE_URL}${item.poster_path}`} alt="list-item-pic" />
+                  <img className="list-item__image" src={item.poster_path ? `${IMAGE_URL}${item.poster_path}` : defaultPoster} alt="list-item-pic" />
                   <div className="list-item__info">
                     <p className="info-title">{item.name}</p>
                     <p className="info-votes">
